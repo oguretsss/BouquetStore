65700b079deab5be969335703a2d9b10a39a80ac
oguretsss <hestheone30@gmail.com>
2017-02-03 09:27:44 +0300

    Add Admin layout, AdminController and main view for administration page

---
+++ BouquetStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using BouquetStore.Domain.Abstract;
+using BouquetStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BouquetStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View(repository.Products);
+        }
+
+        public ViewResult Edit(int productID)
+        {
+            Product product = repository.Products.
+                FirstOrDefault(x => x.ProductID == productID);
+            return View(product);
+        }
+    }
+}
