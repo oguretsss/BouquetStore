bdebfe9ae501b00ce6f5fca50c1acb214025eead
oguretsss <hestheone30@gmail.com>
2017-02-22 01:04:28 +0300

    add InstagramController and partial view

---
+++ BouquetStore.WebUI/Controllers/InstagramController.cs
@@ -0,0 +1,24 @@
+﻿using BouquetStore.WebUI.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BouquetStore.WebUI.Controllers
+{
+    public class InstagramController : Controller
+    {
+        private IInstagramFeed provider;
+        public InstagramController(IInstagramFeed feedProvider)
+        {
+            provider = feedProvider;
+        }
+        // GET: Instagram
+        public PartialViewResult Feed(int quantity = 9)
+        {
+            List<string> photos = provider.GetLatestPhotosFromFeed(quantity, "fruit_is_goood");
+            return PartialView(photos);
+        }
+    }
+}
